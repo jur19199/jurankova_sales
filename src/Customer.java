b145af6fb2ad28e1b12885aa6f797dbe528a78d7
jurankova <jur19199@spengergasse.at>
2019-05-06 16:52:13 +0200

    customer

---
+++ src/Customer.java
@@ -0,0 +1,23 @@
+
+public class Customer
+{
+private int customerId;
+private  String firstname;
+public int getCustomerId()
+{
+	return customerId;
+}
+public void setCustomserId(int customerId)
+{
+	this.customerId = customerId;
+}
+public String getFirstname()
+{
+	return firstname;
+}
+public void setFirstname(String firstname)
+{
+	this.firstname = firstname;
+}
+
+}
